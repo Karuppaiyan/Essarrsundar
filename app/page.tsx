2b1403878b267d1b08014203c1e08375b8f9ed5a
Kkaruppaiyan <ukaruppaiyan@gmmil.com>
2026-06-11 13:42:45 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,8 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <>
+
+    <h1 className="text-4xl font-bold text-center mt-8">Welcome to My Portfolio</h1>
     <Header/>
     <HeroCarousel/>
     <PhilosophySection/>
