fb9ae6060950f72c301e67858413966ace58b9e9
Kkaruppaiyan <ukaruppaiyan@gmmil.com>
2026-06-13 14:15:38 +0530

    five commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,38 +9,11 @@ import {
   Footer,
 } from '@/components';
 
-
-
 import type { NextPage } from 'next';
 import AquaticCarousel from "@/components/AquaticCarousel";
 import Testimonials from "@/components/Testimonial";
 
 const Home: NextPage = () => {
-  const testimonialsData = [
-      {
-        name: "Ravi Kumar",
-        role: "Event Manager",
-        company: "ESS ARR Enterprises",
-        image: "https://via.placeholder.com/150",
-        quote:
-          "The LED screens were top-notch and made our event unforgettable!",
-      },
-      {
-        name: "Priya Sharma",
-        role: "Marketing Head",
-        company: "Shivam Video",
-        quote:
-          "Professional service and excellent quality. Highly recommended!",
-      },
-      {
-        name: "Arjun Mehta",
-        role: "CEO",
-        company: "TechVision",
-        image: "https://via.placeholder.com/150",
-        quote:
-          "Reliable, innovative, and always on time. A great partner to work with.",
-      },
-    ];
 
   return (
     <>    
@@ -49,7 +22,7 @@ const Home: NextPage = () => {
     <AquaticCarousel/>
     <PhilosophySection/>
     <StatsSection/>
-     <Testimonials testimonials={testimonialsData} />
+    <Testimonials />
     <SkillsSection/>
     <ContactSection/>
     <Footer/>
