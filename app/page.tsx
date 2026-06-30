b8347afb477dc57fa34c98955480fab808e7d49d
Kkaruppaiyan <ukaruppaiyan@gmmil.com>
2026-07-01 00:23:15 +0530

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,20 @@
-import Image from "next/image";
 import {
   Header,
-  HeroCarousel,
   PhilosophySection,
   StatsSection,
   SkillsSection,
   ContactSection,
   Footer,
 } from '@/components';
-
-import { Amplify } from 'aws-amplify';
-import config from '@/aws-exports';
-
-Amplify.configure(config);
-
 import type { NextPage } from 'next';
 import AquaticCarousel from "@/components/AquaticCarousel";
 import Testimonials from "@/components/Testimonial";
-
-
-
 
 const Home: NextPage = () => {
 
   return (
     <>    
     <Header/> 
-    {/* <HeroCarousel/>    */}
     <AquaticCarousel/>
     <PhilosophySection/>
     <StatsSection/>
