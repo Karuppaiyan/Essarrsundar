f954b8f4ede8c693d1b0f0e042b4cfb696606ec0
Karuppaiyan <ukaruppaiyan@gmail.com>
2026-06-30 23:36:23 +0530

    upload page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,43 @@
-"use client";
+import Image from "next/image";
+import {
+  Header,
+  HeroCarousel,
+  PhilosophySection,
+  StatsSection,
+  SkillsSection,
+  ContactSection,
+  Footer,
+} from '@/components';
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from 'aws-amplify';
+import config from '@/aws-exports';
 
-Amplify.configure(outputs);
+Amplify.configure(config);
 
-const client = generateClient<Schema>();
+import type { NextPage } from 'next';
+import AquaticCarousel from "@/components/AquaticCarousel";
+import Testimonials from "@/components/Testimonial";
 
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
 
-  useEffect(() => {
-    listTodos();
-  }, []);
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+const Home: NextPage = () => {
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+    <>    
+    <Header/> 
+    {/* <HeroCarousel/>    */}
+    <AquaticCarousel/>
+    <PhilosophySection/>
+    <StatsSection/>
+    <Testimonials />
+    <SkillsSection/>
+    <ContactSection/>
+    <Footer/>
+
+    </>
   );
-}
+};
+
+export default Home;
+
